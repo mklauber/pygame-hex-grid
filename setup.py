ffafc8e0742300728a294e1d6df4c5d5fe66d797
Matthew Lauber <matt+github@mklauber.com>
2012-02-24 20:19:04 -0500

    Updated setup script.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 
-setup( 
+setup(
     name='HexMap',
     version='0.1dev',
     packages=['hexmap' ],
@@ -8,5 +8,5 @@ setup(
     author="Matt Lauber",
     author_email="pygame-hexmap@mklauber.com",
     description="A hex map implementation that provides a Map class, a abstract Render class that abstracts most of the drawing requirements, and a couple basic Render subclasses to get things started.",
-    long_description=open( 'README.txt' ).read(),
+    long_description=open( 'README' ).read(),
  )
